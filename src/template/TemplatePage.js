8ac384be61824ea9ec96975fe5312fbe930c6446
Stefan Vitoria <steph.74lw@gmail.com>
2022-12-23 18:41:00 -0100

    workis on first section of template

--- src/template/TemplatePage.js
+++ src/template/TemplatePage.js
@@ -2,33 +2,31 @@ import * as React from 'react';
 import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';
+import {Grid} from '@mui/material';
 
 const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: '#000',
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: 'center',
-  color: theme.palette.text.primary,
+    backgroundColor: '#000',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.primary,
 }));
 
 export default function BasicGrid() {
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={2}>
-        <Grid item xs={12} sm={4}>
-          <Item>xs=8</Item>
-        </Grid>
-        <Grid item xs={12} sm={4}>
-          <Item>xs=4</Item>
-        </Grid>
-        <Grid item xs={12} sm={4} >
-          <Item>xs=4</Item>
-        </Grid>
-        <Grid item xs={12} sm={4}>
-          <Item>xs=8</Item>
-        </Grid>
-      </Grid>
-    </Box>
-  );
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+
+            <Grid container spacing={2}>
+
+                <Grid item xs={12} sm={4}>
+                    <p style={{ textAlign: 'center' }} >Nice p</p>
+                </Grid>
+                <Grid item xs={12} sm={8}>
+                    <Item>xs=4</Item>
+                </Grid>
+
+            </Grid>
+
+        </Box>
+    );
 }
