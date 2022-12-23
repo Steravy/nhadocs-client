165a2f44edf40f4c90b05b0455af4e3aa5a9214d
Stefan Vitoria <steph.74lw@gmail.com>
2022-12-23 21:05:18 -0100

    done first adjustments on personal info section of template

--- src/template/TemplatePage.js
+++ src/template/TemplatePage.js
@@ -1,32 +1,68 @@
 import * as React from 'react';
-import { styled } from '@mui/material/styles';
+// import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import {Grid} from '@mui/material';
+// import Paper from '@mui/material/Paper';
+import { Grid, Stack, Typography, Divider } from '@mui/material';
+import ProfileAvatarComponent from './templateComponents/ProfileAvatarComponent';
 
-const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: '#000',
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: theme.palette.text.primary,
-}));
+
+
+
+
+// const Item = styled(Paper)(({ theme }) => ({
+//     backgroundColor: '#000',
+//     ...theme.typography.body2,
+//     padding: theme.spacing(1),
+//     textAlign: 'center',
+//     color: theme.palette.text.primary,
+// }));
 
 export default function BasicGrid() {
     return (
-        <Box sx={{ flexGrow: 1 }}>
+        <Box sx={{ flexGrow: 1, p: ' 80px 60px' }}>
+            <Divider textAlign="left" >Informacao pessoal</Divider>
 
-            <Grid container spacing={2}>
+            <Grid container spacing={2} sx={{ p: ' 30px 60px' }} >
 
-                <Grid item xs={12} sm={4}>
-                    <p style={{ textAlign: 'center' }} >Nice p</p>
+                <Grid item xs={12} md={3} sx={{ display: 'flex', justifyContent: 'center' }}>
+                    <ProfileAvatarComponent />
                 </Grid>
-                <Grid item xs={12} sm={8}>
-                    <Item>xs=4</Item>
+
+                <Grid item xs={12} md={9}>
+                    <Stack spacing={1}>
+                        <Typography>Stephan Salvatory</Typography>
+                        <Typography>Cabo Verde, Santiago-Praia, Palmarejo, 7600</Typography>
+                        <Typography>+238 9352569</Typography>
+                        <Typography>+238 9567880</Typography>
+                        <Typography>steph.74lw@gmail.com</Typography>
+                        <Typography>Sexo Masculino | Data de nascimento 24 de Outubro de 1996 | Nacionalidade Cabo-
+                            verdiana</Typography>
+                    </Stack>
                 </Grid>
 
             </Grid>
 
+            <Divider textAlign="left" > Educacao e Formacao </Divider>
+
+            <Grid container spacing={2} sx={{ p: ' 30px 60px' }} >
+
+                <Grid item xs={12} md={3} sx={{ display: 'flex', justifyContent: 'center' }}>
+                    <ProfileAvatarComponent />
+                </Grid>
+
+                <Grid item xs={12} md={9}>
+                    <Stack spacing={1}>
+                        <Typography>Stephan Salvatory</Typography>
+                        <Typography>Cabo Verde, Santiago-Praia, Palmarejo, 7600</Typography>
+                        <Typography>+238 9352569</Typography>
+                        <Typography>+238 9567880</Typography>
+                        <Typography>steph.74lw@gmail.com</Typography>
+                        <Typography>Sexo Masculino | Data de nascimento 24 de Outubro de 1996 | Nacionalidade Cabo-
+                            verdiana</Typography>
+                    </Stack>
+                </Grid>
+
+            </Grid>
         </Box>
     );
 }
---
+++ src/template/templateComponents/ProfileAvatarComponent.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Stack from '@mui/material/Stack';
+import theRock from './theRock.jpg'
+
+
+export default function ProfileAvatarComponent() {
+  return (
+    <Stack direction="row" spacing={2}>
+      <Avatar alt="Profile Picture" src={theRock} sx={{height: '200px', width: '200px'}} />
+    </Stack>
+  );
+}
