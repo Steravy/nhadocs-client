eebd05a9550df29dccb52f58eac150ea5fc72879
Stefan Vitoria <steph.74lw@gmail.com>
2022-12-30 12:56:34 -0100

    link to europass guides

--- src/template/templateComponents/LanguageTableComponent.js
+++ src/template/templateComponents/LanguageTableComponent.js
@@ -6,6 +6,10 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import { Typography } from '@mui/material';
+import Link from '@mui/material/Link';
+import { Box } from '@mui/system';
+
 
 function createData(lingua, compreensãoOral, leitura, interacçãoOral, produçãoOral, escrita) {
     return { lingua, compreensãoOral, leitura, interacçãoOral, produçãoOral, escrita };
@@ -14,47 +18,54 @@ function createData(lingua, compreensãoOral, leitura, interacçãoOral, produç
 const rows = [
     createData('Português', 'C2', 'C1', 'C1', 'C1', 'C2'),
     createData('Inglês', 'C1', 'C1', 'C1', 'C1', 'C2'),
-    
+
 ];
 
 export default function LanguageTableComponent() {
     return (
-        <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 650 }} size="small" aria-label="simple table">
-                <TableHead>
-                    <TableRow>
-                        <TableCell align="center" colSpan={1}></TableCell>
-                        <TableCell align="center" colSpan={2}>COMPREENDER</TableCell>
-                        <TableCell align="center" colSpan={2}>FALAR</TableCell>
-                        <TableCell align="center" colSpan={1}>ESCREVER</TableCell>
-                    </TableRow>
-                    <TableRow>
-                        <TableCell align="center">Linguas</TableCell>
-                        <TableCell align="center">Compreensão oral</TableCell>
-                        <TableCell align="center">Leitura</TableCell>
-                        <TableCell align="center">Interacção oral</TableCell>
-                        <TableCell align="center">Produção oral</TableCell>
-                        <TableCell align="center">Produção oral</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {rows.map((row, i) => (
-                        <TableRow
-                            key={i}
-                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                        >
-                            <TableCell align='center' component="th" scope="row">
-                                {row.lingua}
-                            </TableCell>
-                            <TableCell align="center">{row.compreensãoOral}</TableCell>
-                            <TableCell align="center">{row.leitura}</TableCell>
-                            <TableCell align="center">{row.interacçãoOral}</TableCell>
-                            <TableCell align="center">{row.produçãoOral}</TableCell>
-                            <TableCell align="center">{row.escrita}</TableCell>
+        <>
+            <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 650 }} size="small" aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell align="center" colSpan={1}></TableCell>
+                            <TableCell align="center" colSpan={2}>COMPREENDER</TableCell>
+                            <TableCell align="center" colSpan={2}>FALAR</TableCell>
+                            <TableCell align="center" colSpan={1}>ESCREVER</TableCell>
                         </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
+                        <TableRow>
+                            <TableCell align="center">Linguas</TableCell>
+                            <TableCell align="center">Compreensão oral</TableCell>
+                            <TableCell align="center">Leitura</TableCell>
+                            <TableCell align="center">Interacção oral</TableCell>
+                            <TableCell align="center">Produção oral</TableCell>
+                            <TableCell align="center">Produção oral</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {rows.map((row, i) => (
+                            <TableRow
+                                key={i}
+                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                            >
+                                <TableCell align='center' component="th" scope="row">
+                                    {row.lingua}
+                                </TableCell>
+                                <TableCell align="center">{row.compreensãoOral}</TableCell>
+                                <TableCell align="center">{row.leitura}</TableCell>
+                                <TableCell align="center">{row.interacçãoOral}</TableCell>
+                                <TableCell align="center">{row.produçãoOral}</TableCell>
+                                <TableCell align="center">{row.escrita}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            <Box sx={{m: '15px 0'}} >
+                <Link href="https://europa.eu/europass/pt/common-european-framework-reference-language-skills" target="_blank" rel="noopener">
+                    Quadro Europeu Comum de Referência para as Línguas
+                </Link>
+            </Box>
+        </>
     );
 }
