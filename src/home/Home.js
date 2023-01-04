7865f8fa74b6ac32ef0e89ac8ddaf6a30167cc94
Stefan Vitoria <steph.74lw@gmail.com>
2023-01-04 19:24:32 -0100

    added bgVideo

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,10 +1,13 @@
 import { Link } from "@mui/material";
+import BackGroundVideoComponent from "../components/BackGroundVideoComponent";
+
+
 
 export default function Home() {
 
     return (
         <div style={{backgroundColor: 'rgba(246, 246, 244, 1)'}}>
-            
+            <BackGroundVideoComponent />
 
 
             {/* <Link href="/template">Home Page</Link> */}
