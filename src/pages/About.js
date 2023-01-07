d58fccfefff62d89760461a43ef64f23bfd9e181
Stefan Vitoria <steph.74lw@gmail.com>
2023-01-07 21:45:10 -0100

    updates in new version on navBar component

---
+++ src/pages/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { BsNewspaper } from "react-icons/bs";
+
+export default function About() {
+  return (
+    <div className="about">
+      <h1>About</h1>
+      <BsNewspaper className="page-icon" />
+    </div>
+  );
+}
