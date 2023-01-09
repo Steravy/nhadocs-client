46dd5a930776d026a732327e19378c3230abf525
Stefan Vitoria <steph.74lw@gmail.com>
2023-01-09 22:35:05 -0100

    updates

--- src/components/header/NavBarComponent.js
+++ src/components/header/NavBarComponent.js
@@ -14,68 +14,53 @@ export default function NavBarComponent() {
   const closeMobileMenu = () => setClick(false);
 
   return (
-    <AppBar>
-      <IconContext.Provider value={{ color: "#fff" }}>
-        <Container maxWidth="xl">
-          <nav className="navbar">
-            <div className="navbar-container container">
-              <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-                <GiRocketThruster className="navbar-icon" />
-                nhaDocs
-              </Link>
-              <div className="menu-icon" onClick={handleClick}>
-                {click ? <FaTimes /> : <FaBars />}
-              </div>
-              <ul className={click ? "nav-menu active" : "nav-menu"}>
-                <li className="nav-item">
-                  <NavLink
-                    to="/"
-                    className={({ isActive }) =>
-                      "nav-links" + (isActive ? " activated" : "")
-                    }
-                    onClick={closeMobileMenu}
-                  >
-                    Home
-                  </NavLink>
-                </li>
-                <li className="nav-item">
-                  <NavLink
-                    to="/about"
-                    className={({ isActive }) =>
-                      "nav-links" + (isActive ? " activated" : "")
-                    }
-                    onClick={closeMobileMenu}
-                  >
-                    About
-                  </NavLink>
-                </li>
-                <li className="nav-item">
-                  <NavLink
-                    to="/contact"
-                    className={({ isActive }) =>
-                      "nav-links" + (isActive ? " activated" : "")
-                    }
-                    onClick={closeMobileMenu}
-                  >
-                    Contact
-                  </NavLink>
-                </li>
-                <li className="nav-item">
-                  <NavLink
-                    to="/create"
-                    className={({ isActive }) =>
-                      "nav-links" + (isActive ? " activated" : "")
-                    }
-                    onClick={closeMobileMenu}
-                  >
-                    Faq
-                  </NavLink>
-                </li>
-              </ul>
-            </div>
-          </nav>
-        </Container>
-      </IconContext.Provider>
-    </AppBar>
+    <IconContext.Provider value={{ color: "#fff" }}>
+      <nav className="navbar">
+        <div className="navbar-container container">
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+            <GiRocketThruster className="navbar-icon" />
+            nhaDocs
+          </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            {click ? <FaTimes /> : <FaBars />}
+          </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <NavLink
+                to="/about"
+                className={({ isActive }) =>
+                  "nav-links" + (isActive ? " activated" : "")
+                }
+                onClick={closeMobileMenu}
+              >
+                About
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                to="/contact"
+                className={({ isActive }) =>
+                  "nav-links" + (isActive ? " activated" : "")
+                }
+                onClick={closeMobileMenu}
+              >
+                Contact
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                to="/create"
+                className={({ isActive }) =>
+                  "nav-links" + (isActive ? " activated" : "")
+                }
+                onClick={closeMobileMenu}
+              >
+                Faq
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </IconContext.Provider>
   );
 }
